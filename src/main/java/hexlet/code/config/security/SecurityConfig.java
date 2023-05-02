5f59ca1fd47094ad65151cf7e58400a78f8f8223
Maria Komendantova <masha.komendantova@yandex.ru>
2023-05-02 11:08:07 +0300

    fix

--- src/main/java/hexlet/code/config/security/SecurityConfig.java
+++ src/main/java/hexlet/code/config/security/SecurityConfig.java
@@ -29,7 +29,7 @@ import static org.springframework.http.HttpMethod.POST;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    public static final String LOGIN = "/api/login";
+    public static final String LOGIN = "/login";
     public static final List<GrantedAuthority> DEFAULT_AUTHORITIES = List.of(new SimpleGrantedAuthority("USER"));
     private final RequestMatcher publicUrls;
     private final RequestMatcher loginRequest;
@@ -41,12 +41,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                           final UserDetailsService userDetailsService,
                           final PasswordEncoder passwordEncoder,
                           final JWTHelper jwtHelper) {
-        this.loginRequest = new AntPathRequestMatcher(baseUrl + LOGIN, POST.toString());
+        this.loginRequest = new AntPathRequestMatcher("/api" + LOGIN, POST.toString());
         this.publicUrls = new OrRequestMatcher(
                 loginRequest,
-                new AntPathRequestMatcher(baseUrl + UserController.USER_CONTROLLER_PATH, POST.toString()),
-                new AntPathRequestMatcher(baseUrl + UserController.USER_CONTROLLER_PATH, GET.toString()),
-                new NegatedRequestMatcher(new AntPathRequestMatcher(baseUrl + "/**"))
+                new AntPathRequestMatcher("/api" + UserController.USER_CONTROLLER_PATH, POST.toString()),
+                new AntPathRequestMatcher("/api" + UserController.USER_CONTROLLER_PATH, GET.toString()),
+                new NegatedRequestMatcher(new AntPathRequestMatcher("/api" + "/**"))
         );
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
